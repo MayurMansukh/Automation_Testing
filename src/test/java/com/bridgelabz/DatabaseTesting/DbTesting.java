88feee44e0cdc308b3b2dc9a975809469909d2f7
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-28 01:41:50 +0530

    add logic to verify insert,update or delete the data in database testing

--- src/test/java/com/bridgelabz/DatabaseTesting/DbTesting.java
+++ src/test/java/com/bridgelabz/DatabaseTesting/DbTesting.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DbTesting {
     @Test
-    public void testDB() throws ClassNotFoundException, SQLException {
+    public void get_table_data() throws ClassNotFoundException, SQLException {
         //Driver loaded
         Class.forName("com.mysql.cj.jdbc.Driver");
         //Connection created
@@ -12,7 +12,7 @@ public class DbTesting {
         //Statement created
         Statement smt = con.createStatement();
         //Query Executed
-        ResultSet rs = smt.executeQuery("Select * from user ");
+        ResultSet rs = smt.executeQuery("select * from user");
 
         while (rs.next()) {
             int id = rs.getInt(1);
@@ -22,4 +22,47 @@ public class DbTesting {
 
         }
     }
+
+    @Test
+    public void insert_values_into_table() throws ClassNotFoundException, SQLException {
+        //Driver loaded
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Connection created
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_Testing", "root", "1234567890");
+        //Statement created
+        PreparedStatement pst=con.prepareStatement("insert into user values(?,?,?)");
+
+        pst.setInt(1,4);
+        pst.setString(2,"Alex");
+        pst.setString(3,"prime");
+
+        pst.executeUpdate();
+    }
+
+    @Test
+    public void update_values_into_table() throws ClassNotFoundException, SQLException {
+        //Driver loaded
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Connection created
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_Testing", "root", "1234567890");
+        //Statement created
+        PreparedStatement pst=con.prepareStatement("UPDATE user SET name = ? WHERE user_id = ?");
+
+        pst.setString(1,"ramesh");
+        pst.setInt(2,3);
+        pst.executeUpdate();
+    }
+
+    @Test
+    public void delete_row_from_table() throws ClassNotFoundException, SQLException {
+        //Driver loaded
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Connection created
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_Testing", "root", "1234567890");
+        //Statement created
+        PreparedStatement pst=con.prepareStatement("DELETE FROM user WHERE user_id = ?");
+        pst.setInt(1,4);
+        pst.executeUpdate();
+    }
 }
+
